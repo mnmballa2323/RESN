3f1a1d917ad5ea99f0ba76ee791d991b3a448f36
Muneeb Ahmad <muneeb.panwar@gmail.com>
2019-09-24 12:44:08 -0400

    Integrate the theme and add the layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require custom.js
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'agents' => 'pages#agents'
+  get 'single_agent' => 'pages#single_agent'
+  get 'list_rental' => 'pages#list_rental'
+  get 'submit_property' => 'pages#submit_property'
+  get 'property' => 'pages#property'
+  get 'blog' => 'pages#blog'
+  get 'single_blog' => 'pages#single_blog'
+  get 'my_profile' => 'pages#my_profile'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
+  get 'packages' => 'pages#packages'
+  get 'my_property' => 'pages#my_property'
+
 end
