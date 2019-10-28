d31213a3ee292fc6671c2505c0adfdb042d4d177
NabeelMustafa <nabeelmustafa729@gmail.com>
2019-10-28 22:58:04 +0500

    User profile basics

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_121057) do
+ActiveRecord::Schema.define(version: 2019_10_28_140725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "iamge_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "ph_no"
+    t.string "twitter_add"
+    t.string "facebook_add"
+    t.string "linkedin_add"
+    t.string "about_me"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string "title"
@@ -54,4 +69,5 @@ ActiveRecord::Schema.define(version: 2019_10_18_121057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
