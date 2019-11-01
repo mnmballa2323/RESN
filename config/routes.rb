5318ded4c8362b7c9afc36825b543b6bda3e7acb
NabeelMustafa <nabeelmustafa729@gmail.com>
2019-11-01 23:38:46 +0500

    Basic notifcation for chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :notifications
   resources :profiles
   resources :properties
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_181327) do
+ActiveRecord::Schema.define(version: 2019_10_31_190511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_10_30_181327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["profile_id"], name: "index_friend_lists_on_profile_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "descrition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
