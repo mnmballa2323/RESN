cd0988e9ca8ea948d65431c82d8d2f0131211006
Huy Nguyen <huyngvu@gmail.com>
2019-11-27 21:19:21 -0800

    implement solidus

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+
   mount ActionCable.server => '/cable'
   
   resources :notifications
   resources :profiles
   resources :properties
+
+  # This line mounts Solidus's routes at the root of your application.
+  # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
+  # If you would like to change where this engine is mounted, simply change the :at option to something different.
+  #
+  # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
+  mount Spree::Core::Engine, at: '/shop'
+
   devise_for :users
   root 'home#index'
   resources :chats, only: [:index, :show, :create]
